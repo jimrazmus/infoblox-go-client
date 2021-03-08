4c74901bbcc44505501ee3eb78f2952d3a169e6d
Jim Razmus II <jim.razmus@code42.com>
2021-03-08 13:42:29 -0600

    test: Comment out tests to be replaced with standard "testing" module semantics

--- infoblox_go_client_suite_test.go
+++ infoblox_go_client_suite_test.go
@@ -2,12 +2,9 @@ package ibclient_test
 
 import (
 	"testing"
-
-	. "github.com/onsi/ginkgo"
-	. "github.com/onsi/gomega"
 )
 
 func TestInfobloxGoClient(t *testing.T) {
-	RegisterFailHandler(Fail)
-	RunSpecs(t, "InfobloxGoClient Suite")
+	// RegisterFailHandler(Fail)
+	// RunSpecs(t, "InfobloxGoClient Suite")
 }
